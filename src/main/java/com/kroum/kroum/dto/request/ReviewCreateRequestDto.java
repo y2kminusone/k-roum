8e0c9e333216722193bdcc73fd0cafda2e44bd56
y2kminusone <4pril17@naver.com>
2025-05-13 15:31:22 +0900

    feat: Swagger 문서화 전부 완료

--- src/main/java/com/kroum/kroum/dto/request/ReviewCreateRequestDto.java
+++ src/main/java/com/kroum/kroum/dto/request/ReviewCreateRequestDto.java
@@ -13,8 +13,8 @@ import lombok.Setter;
 @Schema(name = "A_ReviewCreateRequestDto", description = "리뷰 작성 요청 DTO")
 public class ReviewCreateRequestDto {
 
-    @Schema(description = "별점, 1 ~ 5", example = "3")
-    private int rating;
+    @Schema(description = "별점, 1 ~ 5 사이의 정수", example = "3")
+    private double rating;
 
     @Schema(description = "리뷰 텍스트", example = "여기 진짜 ㄹㅇ 좋아요")
     private String content;
